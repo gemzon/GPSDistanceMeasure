f62b0396cb4c42a22b82843cd5efbe9596827977
Nikolaj Dam Larsen <nikolaj@excolo.dk>
2019-10-09 22:08:38 +0200

    Refactors measuring algorithms into separate class
    - ...to ease testability
    - Adds a unit test stub for the DistanceMeasurer class

---
+++ GPS_Distance.Tests/DistanceMeasurerTests/MeasureUsingModifiedPythagorous_spec.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace DistanceMeasurerTests.MeasureUsingModifiedPythagorous_spec
+{
+    // Unit test stub
+    public class Given_some_precondition
+    {
+        [Fact]
+        public void Should_do_return_or_produce_a_specific_result()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
