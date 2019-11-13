3aa4b4cd57c6ed5ad813e9fcedc87894c8211836
gemzon <iviatzon@gmail.com>
2019-11-13 19:03:41 +0100

    Failed test data as a reference.

--- GPS_Distance.Tests/DistanceMeasurerTests/MeasureUsingGreaterCircle_spec.cs
+++ GPS_Distance.Tests/DistanceMeasurerTests/MeasureUsingGreaterCircle_spec.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using GPS_Distance.Helpers;
 using GPS_Distance.MeasurementFormulas;
 using GPS_Distance.Models;
 using Xunit;
@@ -9,6 +10,9 @@ namespace DistanceMeasurerTests.MeasureUsingGreaterCircle_spec
     {
         [Theory]
         [InlineData(0, 0, 0, 0, 0)] // NOTE: Relevant test data missing.
+        // Failed test data.. Not sure of the correctness from the web site.
+        //[InlineData(1, 2, 3, 4, 313.7)]                   // From: http://convertalot.com/great_circle_distance_calculator.html
+        //[InlineData(12.34, 43.21, 43.21, 12.34, 4511.25)] // From: http://convertalot.com/great_circle_distance_calculator.html
         public void Should_return_correct_distance(double startLat, double startLong, double endLat, double endLong, double expectedDistance)
         {
             // Arrange
@@ -16,7 +20,7 @@ namespace DistanceMeasurerTests.MeasureUsingGreaterCircle_spec
             var endLocation = new Location(endLat, endLong);
 
             // Act
-            var actualDistance = GreaterCircle.Measure(startLocation, endLocation);
+            var actualDistance = GreaterCircle.Measure(startLocation, endLocation).ToUnit(Unit.Kilometres, 1);
 
             // Assert
             actualDistance.Should().Be(expectedDistance);
--- GPS_Distance.Tests/DistanceMeasurerTests/MeasureUsingHaversineFormula_spec.cs
+++ GPS_Distance.Tests/DistanceMeasurerTests/MeasureUsingHaversineFormula_spec.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using GPS_Distance.Helpers;
 using GPS_Distance.MeasurementFormulas;
 using GPS_Distance.Models;
 using Xunit;
@@ -9,6 +10,9 @@ namespace DistanceMeasurerTests.HaversineFormula_spec
     {
         [Theory]
         [InlineData(0, 0, 0, 0, 0)] // NOTE: Relevant test data missing.
+        // Failed test data.. Not sure of the correctness from the web site.
+        //[InlineData(1, 2, 3, 4, 314.4)]                // From: https://www.movable-type.co.uk/scripts/latlong.html
+        //[InlineData(12.34, 43.21, 43.21, 12.34, 4532)] // From: https://www.movable-type.co.uk/scripts/latlong.html
         public void Should_return_correct_distance(double startLat, double startLong, double endLat, double endLong, double expectedDistance)
         {
             // Arrange
@@ -16,7 +20,7 @@ namespace DistanceMeasurerTests.HaversineFormula_spec
             var endLocation = new Location(endLat, endLong);
 
             // Act
-            var actualDistance = HaversineFormula.Measure(startLocation, endLocation);
+            var actualDistance = HaversineFormula.Measure(startLocation, endLocation).ToUnit(Unit.Kilometres, 1);
 
             // Assert
             actualDistance.Should().Be(expectedDistance);
