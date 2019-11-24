9f5ffad971a70a7d6cb88b073e32dc8aab9a6d02
Davin Davies <4822063+copperbeardy@users.noreply.github.com>
2019-11-24 12:37:39 +0000

    Update DistanceResultsViewModel.cs

--- GPS_Distance/ViewModels/DistanceResultsViewModel.cs
+++ GPS_Distance/ViewModels/DistanceResultsViewModel.cs
@@ -1,28 +1,36 @@
-﻿using System.Collections.Generic;
+using CommonServiceLocator;
+using DistanceCalculator.Models;
+using GPS_Distance.Events;
+
+using Prism.Events;
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows.Input;
-using CommonServiceLocator;
-using GPS_Distance.Events;
-using GPS_Distance.Models;
-using Prism.Events;
-using static GPS_Distance.Helpers.Helper;
+using static DistanceCalculator.Helpers.Helper;
 
 namespace GPS_Distance.ViewModels
 {
     public class DistanceResultsViewModel : BaseViewModel
     {
         #region Fields
-
+    
         private MeasurementInputs _measurementInputs = new MeasurementInputs();
-        private List<Location> _endLocations = new List<Location>();
-        private Unit _selectedUnit = Unit.Miles;
+        private List<Location> _endLocations = new List<Location>();      
         private ObservableCollection<DistanceResult> _distanceResults = new ObservableCollection<DistanceResult>();
+        private Unit _selectedUnit = Unit.Metres;
         private IEventAggregator _eventAggregator;
-
-        #endregion
+        
+                #endregion
 
         #region Properties
+        public ObservableCollection<DistanceResult> DistanceResults
+        {
+            get => _distanceResults;
+            set => SetProperty(ref _distanceResults, value);
+        }      
+
         public MeasurementInputs MeasurementInputs
         {
             get => _measurementInputs;
@@ -40,11 +48,6 @@ namespace GPS_Distance.ViewModels
             get => _selectedUnit;
             set => SetProperty(ref _selectedUnit, value);
         }
-        public ObservableCollection<DistanceResult> DistanceResults
-        {
-            get => _distanceResults;
-            set => SetProperty(ref _distanceResults, value);
-        }
         #endregion
 
         #region Commands
@@ -59,24 +62,32 @@ namespace GPS_Distance.ViewModels
             _eventAggregator = ServiceLocator.Current.GetInstance<IEventAggregator>();
             _eventAggregator.GetEvent<DistanceResultEvent>().Subscribe(DistanceResultEventHandler);
             GenerateSourceDataCommand = new RelayCommand(GenerateSourceData);
-        }
+                }
+               
 
         private void DistanceResultEventHandler(DistanceResultEventArgs obj)
         {
-            MeasurementInputs = new MeasurementInputs(
-                new Location(obj.InputDTO.StartLocation.Latitude, obj.InputDTO.StartLocation.Longitude));
+            
+            MeasurementInputs = new  MeasurementInputs(                 
+                new Location(obj.InputDTO.StartLocation.Latitude, 
+                obj.InputDTO.StartLocation.Longitude));
 
-            EndPositions.AddRange(obj.InputDTO.EndLocations.Select(inputLocation
+            EndPositions.AddRange(obj.InputDTO.EndLocations.Select(inputLocation 
                 => new Location(inputLocation.Latitude, inputLocation.Longitude)));
             GenerateSourceData();
         }
         #endregion
 
         #region Methods
+   
         private void GenerateSourceData()
         {
-            DistanceResults = MeasureDistance(MeasurementInputs, EndPositions, SelectedUnit);
+            DistanceResults = MeasureDistance(EndPositions, SelectedUnit, MeasurementInputs);
         }
+      
         #endregion
+
+
+
     }
 }
