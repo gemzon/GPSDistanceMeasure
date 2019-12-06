58459a22305413fe9d39df0526defdd024f3883f
gemzon <iviatzon@gmail.com>
2019-12-06 16:00:48 +0100

    Changed order of address fields.

--- MapUtils/Lookup.cs
+++ MapUtils/Lookup.cs
@@ -16,20 +16,55 @@
         {
             var adr = geo.Lookup(latitude, longitude, out _);
 
-            string s1 = "", s2 = "", s3 = ""; // Find out the `right´ order for each group.
+            string grp1 = "", grp2 = "", grp3 = "", grp5 = "", grp6 = ""; // Find out the `right´ order for each (G)roup.
 
-            if (adr.Village != "") s1 = adr.Village + " (v)"; // Temp (_).
-            else if (adr.Town != "") s1 = adr.Town + " (t)";
-            else if (adr.City != "") s1 = adr.City + " (c)";
+            if (adr.Name != "") grp1 = adr.Name + " (n)"; // Temp (_).
 
-            if (adr.Suburb != "") s2 += adr.Suburb + " (s)";
-            else if (adr.Country != "") s2 += adr.County + " (c)";
-            else if (adr.District != "") s2 += adr.District + " (d)";
-            else if (adr.Region != "") s2 += adr.Region + " (r)";
+            if (adr.HouseNumber != "") grp2 = adr.HouseNumber + " (h)";
+            else if  (adr.Road != "") grp2 = adr.Road + " (r)";
+            else if  (adr.PostCode != "") grp2 = adr.PostCode + " (p)";
 
-            if (adr.Country != "") s3 += adr.Country;
+            if (adr.Hamlet != "") grp3 = adr.Hamlet + " (h)";
+            else if (adr.Village != "") grp3 = adr.Village + " (v)";
+            else if (adr.Suburb != "") grp3 = adr.Suburb + " (s)";
+            else if (adr.Town != "") grp3 = adr.Town + " (t)";
+            else if (adr.City != "") grp3 = adr.City + " (c)";
 
-            return $"{s1}, {s2}, {s3}"; // What if strings are empty...
+            if (adr.County != "") grp5 += adr.County + " (c)";
+            else if (adr.District != "") grp5 += adr.District + " (d)";
+            else if (adr.Region != "") grp5 += adr.Region + " (r)";
+            else if (adr.State != "") grp5 += adr.State + " (s)";
+
+            if (adr.Country != "") grp6 += adr.Country;
+
+            return $"{grp1}, {grp2} , {grp3}, {grp5}, {grp6}"; // What if strings are empty... Show the 3 most significant.
         }
     }
 }
+
+/*                  Group   Order   (my personal guess (not native English address understanding))
+---------------------------------
+Pedestrian          0       -
+
+Name                1       ?
+
+HouseNumber         2       1
+Road                2       2
+PostCode            2       3
+
+Hamlet              3       1
+Village             3       2
+Suburb              3       3
+Town                3       4
+City                3       5
+
+Neighborhood        4
+
+County              5
+District            5
+Region              5
+State               5
+
+CountryCode         6
+Country             6
+*/
