2daa7dc936b7e38e923a8192201369d8a58be0a1
gemzon <iviatzon@gmail.com>
2019-12-20 12:44:18 +0100

    List Separator

--- DistanceCalculator/Models/Location.cs
+++ DistanceCalculator/Models/Location.cs
@@ -1,10 +1,13 @@
 ﻿namespace DistanceCalculator.Models
 {
+    using System.Globalization;
     using static DistanceCalculator.Helpers.Helper;
     using static MapUtils.Lookup;
 
+
     public class Location
     {
+        internal readonly string LS = CultureInfo.CurrentCulture.TextInfo.ListSeparator;
         public Location()
         {
         }
@@ -22,8 +25,8 @@
         public double LatitudeRadians { get; }
         public double LongitudeRadians { get; }
         public double EarthRadius => RadiusLatitudeAdjustment(Latitude);
-        public string LocationShort => $"Lat: {Latitude}, Lon: {Longitude}";
-        public string LocationLong => $"Latitude: {Latitude}, Longitude: {Longitude}";
+        public string LocationShort => $"Lat{LS} Lon: {Latitude}{LS} {Longitude}";
+        public string LocationLong => $"Latitude: {Latitude}{LS} Longitude: {Longitude}";
         public string DisplayName => DisplayName(Latitude, Longitude);
         public string ShortName => ShortName(Latitude, Longitude);
     }
